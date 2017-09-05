9bfaa624877f909e2e5db89e3fa97dfc133e82fb
nenad <nesa993@gmail.com>
2017-09-05 19:37:58 +0200

    adding theme for the website

--- contracts/HelpToken.sol
+++ contracts/HelpToken.sol
@@ -3,5 +3,13 @@ pragma solidity ^0.4.4;
 import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 
 contract HelpToken is StandardToken {
-    
+    string public name = "HelpToken";
+    string public token = "HT";
+    uint public decimals = 6;
+    uint public NUM_TOKENS = 1000000; // 1 milion of initial NUM_TOKENS
+
+    function HelpToken() {
+        totalSupply = NUM_TOKENS;
+        balances[msg.sender] = NUM_TOKENS;
+    }
 }
