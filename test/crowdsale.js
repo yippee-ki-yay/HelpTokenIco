42235a17254e67628e8b9237fb36cf4afb23393d
nenad <nesa993@gmail.com>
2017-09-12 13:49:42 +0200

    writing tests for the contracts

--- test/crowdsale.js
+++ test/crowdsale.js
@@ -4,27 +4,48 @@ var HelpToken = artifacts.require("HelpToken");
 contract('Crowdsale', async (accounts) => {
   it("Test if we can buy tokens from the crowd sale", async () => {
    
+    const crowdsaleInstance = await Crowdsale.deployed();
+
+    const tokenContract = await getTokenContract(crowdsaleInstance);
+
+    //buy a 100 HelpTokens
+    const buyToken = await crowdsaleInstance.buyTokens.sendTransaction(accounts[1], {
+                    value: web3.toWei('0.1', 'ether'), 
+                    from: accounts[1]});
+
+    //check the balance
+    const numTokens = await tokenContract.balanceOf(accounts[1]);
+
+    assert.equal(numTokens.valueOf(), 100, "The second account bought a 100 HelpTokens with 0.1 ether");
+
+  });
+
+  it("Test if we can buy tokens by calling the fallback method", async () => {
    
     const crowdsaleInstance = await Crowdsale.deployed();
+
+    const tokenContract = await getTokenContract(crowdsaleInstance);
+
+    //buy a 100 HelpTokens
+    const buyToken = await crowdsaleInstance.sendTransaction({
+                    value: web3.toWei('0.1', 'ether'), 
+                    from: accounts[2]});
+
+    //check the balance
+    const numTokens = await tokenContract.balanceOf(accounts[2]);
+
+    assert.equal(numTokens.valueOf(), 100, "The second account bought a 100 HelpTokens with 0.1 ether");
+
+  });
+
+  //Helper method
+  async function getTokenContract(crowdsaleInstance) {
     const tokenAddress = await crowdsaleInstance.token.call();
 
     const tokenContract = await HelpToken.at(tokenAddress);
 
-    const owner = await tokenContract.owner.call();
-    const ico = await tokenContract.ico.call();
-
-    const buyToken = await crowdsaleInstance.buyTokens.sendTransaction(accounts[0], {
-                    value: web3.toWei('1', 'ether'), 
-                    from: accounts[0]});
-
-    console.log(buyToken);
-
-    const numTokens = await tokenContract.balanceOf(accounts[0]);
-    const icoTokens = await tokenContract.balanceOf(ico);
-
-    console.log(numTokens, icoTokens);
-
-  });
+    return tokenContract;
+  }
  
 
 });
