cbd1e5ba31f9e857cd8c555ea3427cd23ae21c07
nenad <nesa993@gmail.com>
2017-09-12 17:48:31 +0200

    updating the erc20 file

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -5,5 +5,10 @@ contract ERC20 {
      function totalSupply() constant returns (uint totalSupply);
      function balanceOf(address _owner) constant returns (uint balance);
      function transfer(address _to, uint _value) returns (bool success);
+     function allowance(address owner, address spender) constant returns (uint256);
+     function transferFrom(address from, address to, uint256 value) returns (bool);
+     function approve(address spender, uint256 value) returns (bool);
+     
+     event Approval(address indexed owner, address indexed spender, uint256 value);
      event Transfer(address indexed _from, address indexed _to, uint _value);
 }
